bd0a3615d00b8df2f1e849d21d8014bac06689cc
ymirotvorenie <karoline.navarra@gmail.com>
2024-04-04 20:41:09 +0700

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,46 +1,38 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        String user;
+        String user = null;
         int choice = 1;
 
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter."
+                + "\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
         if (s.hasNextInt()) {
             choice = s.nextInt();
             switch (choice) {
                 case 0:
                     return;
                 case 1:
-                    System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
-                    user = getUserInput();
-                    System.out.println("Hello, " + user + "!");
+                    Engine.startGame("", choice);
                     break;
                 case 2:
-                    System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
-                    user = getUserInput();
-                    System.out.println("Hello, " + user + "!");
-                    Even.startGame(user);
+                    Engine.startGame(Even.question, choice);
                     break;
+                case 3:
+                    Engine.startGame(Calc.question, choice);
                 default:
-                    System.out.println("Enter correct number!");
+                    break;
 
             }
-
         } else {
             throw new IllegalArgumentException("Enter number of task!");
         }
-    }
-    public static void greet(String name) {
-        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
-        System.out.println("Hello, " + name);
-    }
-    public static String getUserInput() {
-        Scanner s = new Scanner(System.in);
-        return s.nextLine();
     }
 }
