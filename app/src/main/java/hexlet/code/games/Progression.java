523ced0af1e88f88d160896df3176b206507b453
ymirotvorenie <karoline.navarra@gmail.com>
2024-04-05 09:54:18 +0700

    fix Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -5,12 +5,13 @@ public class Progression {
 
     public static int startGame(int arraySize, int skipPosition, int progressionStep, int startPosition) {
         int answer = 0;
+        System.out.print("Question: ");
         for (int i = 0; i < arraySize; i++) {
             if (i != skipPosition) {
                 System.out.print(startPosition + progressionStep * i + " ");
             } else {
                 answer = startPosition + progressionStep * i;
-                System.out.print("..");
+                System.out.print(".. ");
             }
         }
         return answer;
